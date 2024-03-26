7e39428cc370b2f9ac70dab059d20c88f5ce7131
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-03-26 16:18:29 +0800

    add manifest.scm

---
+++ manifest.scm
@@ -0,0 +1,11 @@
+;; What follows is a "manifest" equivalent to the command line you gave.
+;; You can store it in a file that you may then pass to any 'guix' command
+;; that accepts a '--manifest' (or '-m') option.
+
+(specifications->manifest
+  (list "emacs-next"
+        "pkg-config"
+        "guile"
+        "make"
+        "gcc-toolchain"
+        "bear"))
